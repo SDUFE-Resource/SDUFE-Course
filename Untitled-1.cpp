5ddc2eade94412907c55926d87a79ae42b198a63
SDUFE-Resource <50288430+SDUFE-Resource@users.noreply.github.com>
2019-06-29 20:25:55 +0800

    delete it

---
+++ Untitled-1.cpp
@@ -0,0 +1,4 @@
+gcc (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0
+Copyright (C) 2017 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
